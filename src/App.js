d098b137ad1184d8dcf286e1b7839733fc0b88be
nassir <daniya_n@yahoo.com>
2021-12-10 20:31:00 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [peopleList, setPopleList] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{peopleList.length} birthdays today</h3>
+        <List peopleList={peopleList} />
+        <button onClick={() => setPopleList([])}>clear all</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
